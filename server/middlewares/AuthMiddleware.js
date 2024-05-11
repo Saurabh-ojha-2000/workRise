2ca287398a64651ee2e6f27a49da29d3ad737547
Saurabh Ojha <107386248+Saurabh-ojha-2000@users.noreply.github.com>
2024-05-11 19:52:44 +0530

    Update AuthMiddleware.js

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -1,51 +1,36 @@
-// import jwt from "jsonwebtoken";
-
-// export const verifyToken = async (req, res, next) => {
-//   try {
-//     const tokenObject = JSON.parse(req.cookies.jwt);
-//     const token = tokenObject.jwt;
-//     console.log("Token:", token);
-    
-//     const authKey = process.env.JWT_KEY;
-//     console.log("Key:", authKey);
-
-//     if (!token) {
-//       return res.status(401).send("You are not authenticated!");
-//     }
-
-//     const payload = await jwt.verify(token, authKey);
-//     console.log("Payload:", payload);
-    
-//     req.userId = payload?.userId;
-//     next();
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return res.status(403).send("Token is not valid!");
-//   }
-// };
-
 import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
   try {
-    if (!req.cookies.jwt) {
-      return res.status(401).send("You are not authenticated!");
+    let token;
+
+    // Check if JWT token is in cookies
+    if (req.cookies.jwt) {
+      const tokenObject = JSON.parse(req.cookies.jwt);
+      token = tokenObject.jwt;
     }
 
-    const tokenObject = JSON.parse(req.cookies.jwt);
-    const token = tokenObject.jwt;
-    console.log("Token:", token);
-    
-    const authKey = process.env.JWT_KEY;
-    console.log("Key:", authKey);
+    // If JWT token is not in cookies, check the Authorization header
+    if (!token && req.headers.authorization) {
+      const authHeader = req.headers.authorization;
+      const [, tokenFromHeader] = authHeader.split(" ");
 
+      if (tokenFromHeader) {
+        token = tokenFromHeader;
+      }
+    }
+
+    // If token is still not found, return 401 Unauthorized
     if (!token) {
       return res.status(401).send("You are not authenticated!");
     }
 
+    // Verify the token
+    const authKey = process.env.JWT_KEY;
     const payload = await jwt.verify(token, authKey);
     console.log("Payload:", payload);
-    
+
+    // Attach userId to the request object
     req.userId = payload?.userId;
     next();
   } catch (error) {
